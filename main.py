94bf857043f2e0c244bf6fe4d64fd81bfe8a2fff
NaClSSS <2621554303@qq.com>
2022-11-04 09:54:46 +0800

    11.4 ready

--- main.py
+++ main.py
@@ -1,26 +1,37 @@
+import json
+
 from selenium import webdriver
 import time
 from PIL import Image
+import matplotlib.pyplot as plt
 
+url = 'https://live.bilibili.com/24003948?broadcast_type=0&is_room_feed=1&spm_id_from=333.999.0.0'
 driver = webdriver.Edge('msedgedriver.exe')
-driver.get('https://live.bilibili.com/6366787?hotRank=0&session_id=aceba109cb0675845468a909bdb7022f_62794219-714C-44A4-BAB8-7E16E91393BE&launch_id=1000229')
-# driver.get('https://live.bilibili.com/24003948?broadcast_type=0&is_room_feed=1&spm_id_from=333.999.0.0')
+driver.get(url)
+driver.delete_all_cookies()
+with open('c') as f:
+    c = json.loads(f.read())
+for a in c:
+    driver.add_cookie(a)
+driver.get(url)
+
 input('Begin？')
 print('OK.')
 
 name = ['a.png']
-driver.get_screenshot_as_file(name[0])
 j = 1
 flag = True
 crop = [10, 175, 920, 685]
 while True:
-    time.sleep(5)
+    time.sleep(3.6)
     driver.get_screenshot_as_file(name[0])
     img = Image.open(name[0])
-    img.crop(crop).save('ok/%d.png' % j)
+    t = img.crop(crop).save('ok/%d.png' % j)
     print(crop)
     print('Saved %d' % j)
     if flag:
+        plt.imshow(img.crop(crop))
+        plt.show()
         s = input('offset:x1, y1, x2, y2:')
         if s == 'ok':
             flag = False
